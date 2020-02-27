ca5bb2f9912e2a12a1c41f5fa7d26d60ec196519
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-02-27 18:16:33 +0530

    XAMARIN-33398 - Sample added.

---
+++ SfListViewSample/SfListViewSample/SfListViewSample.Android/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms;
+using Plugin.Permissions;
+using Plugin.CurrentActivity;
+
+namespace SfListViewSample.Droid
+{
+    [Activity(Label = "SfListViewSample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            CrossCurrentActivity.Current.Init(this, bundle);
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
+
---
+++ SfListViewSample/SfListViewSample/SfListViewSample/ViewModel/ViewModel.cs
@@ -0,0 +1,221 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace SfListViewSample
+{
+    [Preserve(AllMembers = true)]
+    public class ViewModel
+    {
+        #region Fields
+
+        private Random random = new Random();
+
+        #endregion
+
+        #region Constructor
+
+        public ViewModel()
+        {
+            GenerateContactDetails(80);
+        }
+
+        #endregion
+
+        private ObservableCollection<ListViewContactsInfo> customerDetails;
+
+        public ObservableCollection<ListViewContactsInfo> ContactsInfo
+        {
+            get { return customerDetails; }
+            set { this.customerDetails = value; }
+        }
+
+
+        #region Get Contacts Details
+
+        public async void GenerateContactDetails(int count)
+        {
+            customerDetails = new ObservableCollection<ListViewContactsInfo>();
+            ListViewContactsInfo details;
+            for (int i = 0; i < 3; i++)
+            {
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "Media\\Pictures",
+                    Name = "Image"+i,
+                    SaveToAlbum = false,
+                    CompressionQuality = 75,
+                    CustomPhotoSize = 50,
+                    MaxWidthHeight = 2000,
+                });
+                details = new ListViewContactsInfo()
+                {
+                    ContactType = contactType[random.Next(0, 5)],
+                    ContactNumber = random.Next(100, 400).ToString() + "-" + random.Next(500, 800).ToString() + "-" + random.Next(1000, 2000).ToString(),
+                    ContactName = CustomerNames[0],
+                    ContactImage = ImageSource.FromStream(() =>
+                    {
+                        if (file == null)
+                            return null;
+                        var stream = file.GetStream();
+                        file.Dispose();
+                        return stream;
+                    })
+            };
+                customerDetails.Add(details);
+            }
+        }
+
+        #endregion
+
+        #region Contacts Information
+
+        string[] contactType = new string[]
+        {
+            "HOME",
+            "WORK",
+            "MOBILE",
+            "OTHER",
+            "BUSINESS"
+        };
+
+        string[] CustomerNames = new string[]
+        {
+            "Kyle",
+            "Gina",
+            "Irene",
+            "Katie",
+            "Michael",
+            "Oscar",
+            "Ralph",
+            "Torrey",
+            "William",
+            "Bill",
+            "Daniel",
+            "Frank",
+            "Brenda",
+            "Danielle",
+            "Fiona",
+            "Howard",
+            "Jack",
+            "Larry",
+            "Holly",
+            "Jennifer",
+            "Liz",
+            "Pete",
+            "Steve",
+            "Vince",
+            "Zeke",
+            "Aiden",
+            "Jackson",
+            "Mason  ",
+            "Liam   ",
+            "Jacob  ",
+            "Jayden ",
+            "Ethan  ",
+            "Noah   ",
+            "Lucas  ",
+            "Logan  ",
+            "Caleb  ",
+            "Caden  ",
+            "Jack   ",
+            "Ryan   ",
+            "Connor ",
+            "Michael",
+            "Elijah ",
+            "Brayden",
+            "Benjamin",
+            "Nicholas",
+            "Alexander",
+            "William",
+            "Matthew",
+            "James  ",
+            "Landon ",
+            "Nathan ",
+            "Dylan  ",
+            "Evan   ",
+            "Luke   ",
+            "Andrew ",
+            "Gabriel",
+            "Gavin  ",
+            "Joshua ",
+            "Owen   ",
+            "Daniel ",
+            "Carter ",
+            "Tyler  ",
+            "Cameron",
+            "Christian",
+            "Wyatt  ",
+            "Henry  ",
+            "Eli    ",
+            "Joseph ",
+            "Max    ",
+            "Isaac  ",
+            "Samuel ",
+            "Anthony",
+            "Grayson",
+            "Zachary",
+            "David  ",
+            "Christopher",
+            "John   ",
+            "Isaiah ",
+            "Levi   ",
+            "Jonathan",
+            "Oliver ",
+            "Chase  ",
+            "Cooper ",
+            "Tristan",
+            "Colton ",
+            "Austin ",
+            "Colin  ",
+            "Charlie",
+            "Dominic",
+            "Parker ",
+            "Hunter ",
+            "Thomas ",
+            "Alex   ",
+            "Ian    ",
+            "Jordan ",
+            "Cole   ",
+            "Julian ",
+            "Aaron  ",
+            "Carson ",
+            "Miles  ",
+            "Blake  ",
+            "Brody  ",
+            "Adam   ",
+            "Sebastian",
+            "Adrian ",
+            "Nolan  ",
+            "Sean   ",
+            "Riley  ",
+            "Bentley",
+            "Xavier ",
+            "Hayden ",
+            "Jeremiah",
+            "Jason  ",
+            "Jake   ",
+            "Asher  ",
+            "Micah  ",
+            "Jace   ",
+            "Brandon",
+            "Josiah ",
+            "Hudson ",
+            "Nathaniel",
+            "Bryson ",
+            "Ryder  ",
+            "Justin ",
+            "Bryce  ",
+        };
+
+        #endregion
+    }
+}
